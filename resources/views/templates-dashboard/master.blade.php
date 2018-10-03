dc713ba149741b51d9c6eaf4c19490b2db983bfd
tooshort06 <christophervistal24@gmail.com>
2018-10-04 01:47:54 +0800

    With Partial Dashboard

---
+++ resources/views/templates-dashboard/master.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags-->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="au theme template">
+        <meta name="author" content="Hau Nguyen">
+        <meta name="keywords" content="au theme template">
+        <!-- Title Page-->
+        <title>Dashboard 2</title>
+        <!-- Fontfaces CSS-->
+        <link href="/dashboard/css/font-face.css" rel="stylesheet" media="all">
+        <link href="/dashboard/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+        <link href="/dashboard/vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+        <link href="/dashboard/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+        <!-- Bootstrap CSS-->
+        <link href="/dashboard/vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+        <!-- Vendor CSS-->
+        <link href="/dashboard/vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+        <link href="/dashboard/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+        <link href="/dashboard/vendor/wow/animate.css" rel="stylesheet" media="all">
+        <link href="/dashboard/vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+        <link href="/dashboard/vendor/slick/slick.css" rel="stylesheet" media="all">
+        <link href="/dashboard/vendor/select2/select2.min.css" rel="stylesheet" media="all">
+        <link href="/dashboard/vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
+        <link href="/dashboard/vendor/vector-map/jqvmap.min.css" rel="stylesheet" media="all">
+        <!-- Main CSS-->
+        <link href="/dashboard/css/theme.css" rel="stylesheet" media="all">
+    </head>
+    <body class="animsition">
+        <section>
+            @yield('content')
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="copyright">
+                            <p>Copyright © 2018 Colorlib. All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- END PAGE CONTAINER-->
+    </div>
+</div>
+<!-- Jquery JS-->
+<script src="/dashboard/vendor/jquery-3.2.1.min.js"></script>
+<!-- Bootstrap JS-->
+<script src="/dashboard/vendor/bootstrap-4.1/popper.min.js"></script>
+<script src="/dashboard/vendor/bootstrap-4.1/bootstrap.min.js"></script>
+<!-- Vendor JS       -->
+<script src="/dashboard/vendor/slick/slick.min.js">
+</script>
+<script src="/dashboard/vendor/wow/wow.min.js"></script>
+<script src="/dashboard/vendor/animsition/animsition.min.js"></script>
+<script src="/dashboard/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+</script>
+<script src="/dashboard/vendor/counter-up/jquery.waypoints.min.js"></script>
+<script src="/dashboard/vendor/counter-up/jquery.counterup.min.js">
+</script>
+<script src="/dashboard/vendor/circle-progress/circle-progress.min.js"></script>
+<script src="/dashboard/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="/dashboard/vendor/chartjs/Chart.bundle.min.js"></script>
+<script src="/dashboard/vendor/select2/select2.min.js">
+</script>
+<script src="/dashboard/vendor/vector-map/jquery.vmap.js"></script>
+<script src="/dashboard/vendor/vector-map/jquery.vmap.min.js"></script>
+<script src="/dashboard/vendor/vector-map/jquery.vmap.sampledata.js"></script>
+<script src="/dashboard/vendor/vector-map/jquery.vmap.world.js"></script>
+<!-- Main JS-->
+<script src="/dashboard/js/main.js"></script>
+</body>
+</html>
+<!-- end document-->
