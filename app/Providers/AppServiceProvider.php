4026c16941051f4e349aab96a2e07cf1828578ed
='christopher <christophervistal24@gmail.com>
2019-02-11 00:23:56 +0800

    Add some CONFIG VARIABLES

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,9 +13,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         Schema::defaultStringLength(191);
+        if (env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
     }
 
     /**
@@ -24,7 +28,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-
+        if (env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS',true);
+        }
 
     }
 }
